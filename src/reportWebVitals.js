ae6c9e0a36b97fb8806df407fb347935a3806e81
unknown <youssefmakrem01@gmail.com>
2026-04-20 17:25:06 +0100

    fix: add lucide-react

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,12 +1,12 @@
+import { onCLS, onFCP, onINP, onLCP, onTTFB } from 'web-vitals';
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    onCLS(onPerfEntry);
+    onINP(onPerfEntry); // Replaces onFID in newer versions
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
   }
 };
 
